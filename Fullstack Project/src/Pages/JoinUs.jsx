808a0a07c59c82dc4c0f86996578fbe168e83228
jmmarcuis <marcosjhainnoallrick@gmail.com>
2024-01-23 08:06:53 +0800

    hello

--- Fullstack Project/src/Pages/JoinUs.jsx
+++ Fullstack Project/src/Pages/JoinUs.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "../Pages/Page Styles/JoinUs.css";
 import NavBar from "../Components/navBar";
 import Footer from "../Components/Footer";
@@ -16,15 +15,41 @@ const JoinUs = () => {
 
       <div className="join-us-main-section">
         <div className="main-section-wrapper">
-            <div className="main-section-title"><h1>Join Us</h1></div>
-            <div className="main-section-image-container">
-                <img src="" alt="" />
-            </div>
+          <div className="main-section-title">
+            <h1>Join Us</h1>
+          </div>
+          <div className="main-section-image-container">
+            <img src="" alt="" />
+          </div>
         </div>
       </div>
-      <div className="join-us-ministry-section"></div>
-      <div className="join-us-volunteer-section"></div>
-      <div className="join-us-connect-group"></div>
+      <div className="join-us-ministry-section">
+        <div className="ministry-title-container">
+          <h1>MINISTRIES</h1>
+        </div>
+
+        <div className="ministry-wrapper">
+
+        </div>
+      </div>
+      <div className="join-us-volunteer-section">
+      <div className="ministry-title-container">
+          <h1>MINISTRIES</h1>
+        </div>
+
+        <div className="ministry-wrapper">
+            
+        </div>
+      </div>
+      <div className="join-us-connect-group">
+      <div className="ministry-title-container">
+          <h1>MINISTRIES</h1>
+        </div>
+
+        <div className="ministry-wrapper">
+            
+        </div>
+      </div>
 
       <div className="prayer-footer-container">
         <Footer />
