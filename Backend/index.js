27eca347161f6f95fced48ab5aadcec18131d128
Angela <74482789+nglbrqz@users.noreply.github.com>
2024-02-08 16:54:12 +0800

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const{mongoose} = require('mongoose')
 const app = express();
 app.use(cors(
     {
-        origin: ["https://newlife-southwoods.vercel.app"],
+        //origin: ["https://newlife-southwoods.vercel.app"],
         // methods: ["POST", "GET"],
         origin: 'http://localhost:5173',
         credentials: true
