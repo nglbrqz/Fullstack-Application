f22092e49cdaf2c74bbca418342ee6d078439bd1
Angela <74482789+nglbrqz@users.noreply.github.com>
2024-02-08 16:54:53 +0800

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -23,7 +23,8 @@ import RegisterAdmin from "./Pages/RegisterAdmin";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://newlife-southwoods.vercel.app";
+//axios.defaults.baseURL = "https://newlife-southwoods.vercel.app";
+axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 function App() {
