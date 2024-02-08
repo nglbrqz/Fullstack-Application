71aad0ac1d072439185f74136c97257bfcb1dd07
Angela <74482789+nglbrqz@users.noreply.github.com>
2024-02-08 21:53:40 +0800

    Update eventController.js tmp

--- Backend/controllers/eventController.js
+++ Backend/controllers/eventController.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
  
 const storage = multer.diskStorage({
-  destination: "./tmp/uploads/",
+  destination: "/tmp/uploads/",
   filename: function (req, file, cb) {
     cb(
       null,
