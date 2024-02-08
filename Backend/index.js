a87ec1532944332f2ba1f7dcc373204f2121d703
Angela <74482789+nglbrqz@users.noreply.github.com>
2024-02-08 22:00:52 +0800

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Set up CORS
 app.use(cors({
-        origin: ["https://newlife-southwoods.vercel.app/events"],
+        origin: ["https://newlife-southwoods.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         //origin: 'http://localhost:5173',
         credentials: true
